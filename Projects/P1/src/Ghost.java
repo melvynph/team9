e678cede9791c9f9407f5d281e738af51ff27781
Sarah Bang <sbang@umd.edu>
2021-01-06 21:26:26 -0500

    Completed ghosts get_valid_moves()

--- Projects/P1/src/Ghost.java
+++ Projects/P1/src/Ghost.java
@@ -13,7 +13,26 @@ public class Ghost{
 	}
 
 	public ArrayList<Location> get_valid_moves() {
-		return null;
+		ArrayList<Location> ans  = new ArrayList<>();
+
+		Location left = new Location(myLoc.x - 1, myLoc.y);
+		Location right = new Location(myLoc.x + 1, myLoc.y);
+		Location up = new Location(myLoc.x, myLoc.y - 1);
+		Location down = new Location(myLoc.x, myLoc.y + 1);
+		if (!(myMap.getLoc(left).contains(Map.Type.WALL)) && (left.x >= 0)) {	// left
+			ans.add(left);
+		}
+		if (!(myMap.getLoc(right).contains(Map.Type.WALL)) && (right.x <= 30)) {		// right
+			ans.add(right);
+		}
+		if (!(myMap.getLoc(up).contains(Map.Type.WALL)) && (up.y >= 0)) {	// up
+			ans.add(up);
+		}
+		if (!(myMap.getLoc(down).contains(Map.Type.WALL)) && (down.y <= 30)) {	// down
+			ans.add(down);
+		}
+
+		return ans;
 	}
 
 	public boolean move() {
