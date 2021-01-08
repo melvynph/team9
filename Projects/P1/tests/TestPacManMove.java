728792609761e557c54bd66ca56a90af6dabc7d2
Zeeshan Shaikh <zshaikh322@gmail.com>
2021-01-08 17:06:49 -0500

    fixed location of pacman

--- Projects/P1/tests/TestPacManMove.java
+++ Projects/P1/tests/TestPacManMove.java
@@ -7,17 +7,15 @@ public class TestPacManMove extends TestCase {
 	public void testPacManMove() throws FileNotFoundException{
 		NoFrame frame = new NoFrame(); //Creates A New Map With Walls and Tokens w/o a Display
 
-		Pacman pacman = frame.addPacMan(new Location(15,15));
-		WallComponent wall1 = new WallComponent(15,14, 20);
-		WallComponent wall2 = new WallComponent(15,16, 20);
-		WallComponent wall3 = new WallComponent(14,15, 20);
-		WallComponent wall4 = new WallComponent(16,15, 20);
+		Pacman pacman = frame.addPacMan(new Location(1,1));
+		WallComponent wall1 = new WallComponent(2,1, 20);
+		WallComponent wall2 = new WallComponent(1,2, 20);
 		//Pacman cannot move because walls on all sides
 		assertFalse(pacman.move());
 	
 		NoFrame validMove = new NoFrame();
-		Pacman pacman2 = validMove.addPacman(new Location(15, 15));
-		//Pacman can move anywhere
+		Pacman pacman2 = validMove.addPacman(new Location(1, 1));
+		//Pacman can move up or right
 		assertTrue(pacman2.move());
 
 	}
