92334f7170075691b0c89debf6abda9b8433c66c
Zeeshan Shaikh <zshaikh322@gmail.com>
2021-01-08 17:40:30 -0500

    typo on PacMan

--- Projects/P1/tests/TestPacManMove.java
+++ Projects/P1/tests/TestPacManMove.java
@@ -7,14 +7,14 @@ public class TestPacManMove extends TestCase {
 	public void testPacManMove() throws FileNotFoundException{
 		NoFrame frame = new NoFrame(); //Creates A New Map With Walls and Tokens w/o a Display
 
-		Pacman pacman = frame.addPacMan(new Location(1,1));
+		PacMan pacman = frame.addPacMan(new Location(1,1));
 		WallComponent wall1 = new WallComponent(2,1, 20);
 		WallComponent wall2 = new WallComponent(1,2, 20);
 		//Pacman cannot move because walls on all sides
 		assertFalse(pacman.move());
 	
 		NoFrame validMove = new NoFrame();
-		Pacman pacman2 = validMove.addPacman(new Location(1, 1));
+		PacMan pacman2 = validMove.addPacMan(new Location(1, 1));
 		//Pacman can move up or right
 		assertTrue(pacman2.move());
 
