e4556376d0a10cc574e90ebbfa595e7e741a8251
Zeeshan Shaikh <zshaikh322@gmail.com>
2021-01-08 22:27:41 -0500

    small change

--- Projects/P1/src/PacMan.java
+++ Projects/P1/src/PacMan.java
@@ -21,7 +21,7 @@ public class PacMan{
 	public boolean move() {
 		ArrayList<Location> loc = this.get_valid_moves();
 		if (loc.size() > 0) {
- 			this.myLoc = loc.get(0); 
+			this.myLoc = loc.get(0);
 			return true;
 		}
 		return false;
