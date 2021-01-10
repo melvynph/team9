fc3ad25666209a7ed58bb266c518ebf4bc7e013a
anjali1020 <anjalistar15@gmail.com>
2021-01-10 03:23:23 -0500

    ghost_in_range

--- Projects/P1/src/PacMan.java
+++ Projects/P1/src/PacMan.java
@@ -27,7 +27,25 @@ public class PacMan{
 		return false;
 	}
 
-	public boolean is_ghost_in_range() { 
+	public boolean is_ghost_in_range() {
+		Location left = new Location(myLoc.x - 1, myLoc.y);
+		Location right = new Location(myLoc.x + 1, myLoc.y);
+		Location up = new Location(myLoc.x, myLoc.y - 1);
+		Location down = new Location(myLoc.x, myLoc.y + 1);
+		Location curr = new Location(myLoc.x, myLoc.y);
+
+		if(myMap.getLoc(left).contains(Map.Type.GHOST) && myMap.getLoc(curr).contains(Map.Type.PACMAN)){
+			return true;
+		}
+		if(myMap.getLoc(right).contains(Map.Type.GHOST) && myMap.getLoc(curr).contains(Map.Type.PACMAN)){
+			return true;
+		}
+		if(myMap.getLoc(up).contains(Map.Type.GHOST) && myMap.getLoc(curr).contains(Map.Type.PACMAN)){
+			return true;
+		}
+		if(myMap.getLoc(down).contains(Map.Type.GHOST) && myMap.getLoc(curr).contains(Map.Type.PACMAN)){
+			return true;
+		}
 		return false;
 	}
 
