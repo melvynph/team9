f995c6cb7535212ae8add03d39b474272b1c54a0
rongmd <46975033+rongmd@users.noreply.github.com>
2021-01-11 14:23:19 -0500

    Update TestMapEatCookie.java

--- Projects/P1/tests/TestMapEatCookie.java
+++ Projects/P1/tests/TestMapEatCookie.java
@@ -4,15 +4,14 @@ import junit.framework.*;
 import java.awt.Color;
 import java.io.*;
 
-
 public class TestMapEatCookie {
-
+	
 	public void testMapEatCookie() {
 		Frame frame = new Frame();
-		PacMan pacman1 = frame.addPacMan(new Location(0, 0));
-		assertTrue(frame.getMap().eatCookie("pacman1") == null);
+		PacMan pacman = frame.addPacMan(new Location(0, 0));
+		assertTrue(frame.getMap().eatCookie("pacman") == null);
 		pacman.myLoc.shift(1, 1);
-		assertFalse(frame.getMap().eatCookie("pacman1") == null);
+		assertFalse(frame.getMap().eatCookie("pacman") == null);
 		//return null;
 	}
 }
