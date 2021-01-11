4f14a374dfb04a83b49edb228e36b834c515ae9f
Anjali Sharma <63084159+anjali1020@users.noreply.github.com>
2021-01-11 15:20:11 -0500

    Update TestPacManInRange.java

--- Projects/P1/tests/TestPacManInRange.java
+++ Projects/P1/tests/TestPacManInRange.java
@@ -5,6 +5,11 @@ import java.io.*;
 public class TestPacManInRange extends TestCase {
 
 	public void testPacManInRange() throws FileNotFoundException{
-		return null;
+		NoFrame frame = new NoFrame();
+
+		PacMan pacman = frame.addPacMan(new Location(1,1));
+		Ghost ghost = frame.addGhost(new Location(1,2), "ghost", Color.red);
+
+		assertTrue(ghost.is_pacman_in_range());
 	}
 }
