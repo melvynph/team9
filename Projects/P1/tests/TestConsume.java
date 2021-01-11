1ea86f9ab2c1cfa65ae648475f36ad31dff29942
rongmd <rli23@terpmail.umd.edu>
2021-01-11 14:10:55 -0500

    adds JUnit test for consume() method

--- Projects/P1/tests/TestConsume.java
+++ Projects/P1/tests/TestConsume.java
@@ -1,10 +1,15 @@
+import org.junit.Assert.assertTrue;
 import junit.framework.*;
 import java.awt.Color;
 import java.io.*;
 
+
 public class TestConsume extends TestCase {
 
 	public void testConsume() throws FileNotFoundException {
-		return null;	
+		Frame frame = new Frame();
+		PacMan pacman1 = frame.addPacMan(new Location(0, 0));
+		assertTure(pacman1.consume() == null);
+		//return null;	
 	}
 }
