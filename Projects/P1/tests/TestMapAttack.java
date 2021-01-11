7710e862f764395eec270d3853e6a049c08f39ab
rongmd <46975033+rongmd@users.noreply.github.com>
2021-01-11 14:22:36 -0500

    Update TestMapAttack.java

--- Projects/P1/tests/TestMapAttack.java
+++ Projects/P1/tests/TestMapAttack.java
@@ -3,13 +3,12 @@ import junit.framework.*;
 import org.junit.Assert.assertTrue;
 import org.junit.Assert.assertFalse;
 
-
 public class TestMapAttack {
-
+	
 	public void testMapAttack() {
 		Frame frame = new Frame();
-		PacMan pacman1 = frame.addPacMan(new Location(0, 0));
-		Ghost ghost1 = frame.addGhost(new Location(2, 2), "ghost", Color.blue);
+		PacMan pacman = frame.addPacMan(new Location(0, 0));
+		Ghost ghost = frame.addGhost(new Location(2, 2), "ghost", Color.blue);
 		assertFalse(frame.getMap().attack("ghost") == True);
 		pacman.myLoc.shift(1, 1);
 		assertTrue(frame.getMap().attack("ghost")) == True;
