3a725a57f4ac439ac495a34cff2dad127fc437f3
rongmd <rli23@terpmail.umd.edu>
2021-01-11 13:15:46 -0500

    adds JUnit test for eatCookie() method

--- Projects/P1/tests/TestMapEatCookie.java
+++ Projects/P1/tests/TestMapEatCookie.java
@@ -1,11 +1,12 @@
+import org.junit.Assert.assertFalse;
+import org.junit.Assert.assertTrue;
 import junit.framework.*;
 import java.awt.Color;
 import java.io.*;
-import org.junit.Assert.assertFalse;
-import org.junit.Assert.assertTrue;
+
 
 public class TestMapEatCookie {
-	
+
 	public void testMapEatCookie() {
 		Frame frame = new Frame();
 		PacMan pacman1 = frame.addPacMan(new Location(0, 0));
