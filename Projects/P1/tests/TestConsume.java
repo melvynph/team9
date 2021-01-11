1d638500b892263d6d471f15bdadc80ec83be2a1
rongmd <46975033+rongmd@users.noreply.github.com>
2021-01-11 14:24:23 -0500

    Update TestConsume.java

--- Projects/P1/tests/TestConsume.java
+++ Projects/P1/tests/TestConsume.java
@@ -3,13 +3,12 @@ import junit.framework.*;
 import java.awt.Color;
 import java.io.*;
 
-
 public class TestConsume extends TestCase {
-
+	
 	public void testConsume() throws FileNotFoundException {
 		Frame frame = new Frame();
-		PacMan pacman1 = frame.addPacMan(new Location(0, 0));
-		assertTure(pacman1.consume() == null);
+		PacMan pacman = frame.addPacMan(new Location(0, 0));
+		assertTure(pacman.consume() == null);
 		//return null;	
 	}
 }
