13819877d5186c9a614cb0e38627363d0e091f2f
sarahbangg <57418137+sarahbangg@users.noreply.github.com>
2021-01-11 15:50:26 -0500

    Pacman get valid moves (#5)
    * Updated get_valid_moves PACMAN function
    
    * pacman get_valid_moves
    
    * pacman get_valid_moves
    
    * TestPacManValidMoves completed
    
    * updated test
    
    * Added diagonals
    
    * final push, testing completed
    
    Co-authored-by: Sarah Bang <sbang@umd.edu>
    Co-authored-by: Sarah Bang <sbang@terpmail.umd.edu>

--- Projects/P1/src/PacMan.java
+++ Projects/P1/src/PacMan.java
@@ -15,7 +15,44 @@ public class PacMan{
 	}
 
 	public ArrayList<Location> get_valid_moves() {
-		return null;	
+		// Initialization
+		ArrayList<Location> ans  = new ArrayList<>();
+
+		Location left = new Location(myLoc.x - 1, myLoc.y);
+		Location right = new Location(myLoc.x + 1, myLoc.y);
+		Location up = new Location(myLoc.x, myLoc.y - 1);
+		Location down = new Location(myLoc.x, myLoc.y + 1);
+		Location ne = new Location(myLoc.x - 1, myLoc.y + 1);
+		Location nw = new Location(myLoc.x - 1, myLoc.y - 1);
+		Location se = new Location(myLoc.x + 1, myLoc.y + 1);
+		Location sw = new Location(myLoc.x + 1, myLoc.y - 1);
+
+		if (!(myMap.getLoc(left).contains(Map.Type.WALL)) && (left.x >= 0)) {	// left
+			ans.add(left);
+		}
+		if (!(myMap.getLoc(right).contains(Map.Type.WALL)) && (right.x <= 30)) {		// right
+			ans.add(right);
+		}
+		if (!(myMap.getLoc(up).contains(Map.Type.WALL)) && (up.y >= 0)) {	// up
+			ans.add(up);
+		}
+		if (!(myMap.getLoc(down).contains(Map.Type.WALL)) && (down.y <= 30)) {	// down
+			ans.add(down);
+		}
+		if (!(myMap.getLoc(ne).contains(Map.Type.WALL)) && (ne.y <= 30)) {	// down
+			ans.add(ne);
+		}
+		if (!(myMap.getLoc(nw).contains(Map.Type.WALL)) && (nw.y <= 30)) {	// down
+			ans.add(nw);
+		}
+		if (!(myMap.getLoc(se).contains(Map.Type.WALL)) && (se.y <= 30)) {	// down
+			ans.add(se);
+		}
+		if (!(myMap.getLoc(sw).contains(Map.Type.WALL)) && (sw.y <= 30)) {	// down
+			ans.add(sw);
+		}
+
+		return ans;
 	}
 
 	public boolean move() {
