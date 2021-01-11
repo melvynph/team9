1e7ae254530e4917ed0a2a114b161274df05cfe9
rongmd <rli23@terpmail.umd.edu>
2021-01-11 13:52:16 -0500

    adds JUnit test for attack() method

--- Projects/P1/tests/TestMapAttack.java
+++ Projects/P1/tests/TestMapAttack.java
@@ -1,9 +1,18 @@
 import java.awt.Color;
 import junit.framework.*;
+import org.junit.Assert.assertTrue;
+import org.junit.Assert.assertFalse;
+
 
 public class TestMapAttack {
 
 	public void testMapAttack() {
-		return null;	
+		Frame frame = new Frame();
+		PacMan pacman1 = frame.addPacMan(new Location(0, 0));
+		Ghost ghost1 = frame.addGhost(new Location(2, 2), "ghost", Color.blue);
+		assertFalse(frame.getMap().attack("ghost") == True);
+		pacman.myLoc.shift(1, 1);
+		assertTrue(frame.getMap().attack("ghost")) == True;
+		//return null;
 	}
 }
