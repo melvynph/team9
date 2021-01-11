18fd460ff1e7b906520f78e65b40bff082c7b869
rongmd <rli23@terpmail.umd.edu>
2021-01-11 14:10:03 -0500

    updates consume() method

--- Projects/P1/src/PacMan.java
+++ Projects/P1/src/PacMan.java
@@ -26,7 +26,10 @@ public class PacMan{
 		return false;
 	}
 
-	public JComponent consume() { 
- 		return null;
+	public JComponent consume() {
+		if (myMap.getLoc(myLoc)).contains(Map.Type.COOKIE)
+			return myMap.eatCookie(myName);
+		else
+			return null;
 	}
 }
