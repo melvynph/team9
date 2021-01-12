7f06fe8aea6b4520fc3c81b9ed312e3d19116cdc
Zeeshan Shaikh <zshaikh322@gmail.com>
2021-01-11 20:09:32 -0500

    Update PacMan.java

--- Projects/P1/src/PacMan.java
+++ Projects/P1/src/PacMan.java
@@ -19,33 +19,10 @@ public class PacMan{
 	}
 
 	public boolean move() {
-		ArrayList<Location> loc = this.get_valid_moves();
-		if (loc.size() > 0) {
-			myMap.move(this.myName,loc.get(0),Map.Type.PACMAN);
-			return true;
-		}
 		return false;
 	}
 
-	public boolean is_ghost_in_range() {
-		Location left = new Location(myLoc.x - 1, myLoc.y);
-		Location right = new Location(myLoc.x + 1, myLoc.y);
-		Location up = new Location(myLoc.x, myLoc.y - 1);
-		Location down = new Location(myLoc.x, myLoc.y + 1);
-		Location curr = new Location(myLoc.x, myLoc.y);
-
-		if(myMap.getLoc(left).contains(Map.Type.GHOST) && myMap.getLoc(curr).contains(Map.Type.PACMAN)){
-			return true;
-		}
-		if(myMap.getLoc(right).contains(Map.Type.GHOST) && myMap.getLoc(curr).contains(Map.Type.PACMAN)){
-			return true;
-		}
-		if(myMap.getLoc(up).contains(Map.Type.GHOST) && myMap.getLoc(curr).contains(Map.Type.PACMAN)){
-			return true;
-		}
-		if(myMap.getLoc(down).contains(Map.Type.GHOST) && myMap.getLoc(curr).contains(Map.Type.PACMAN)){
-			return true;
-		}
+	public boolean is_ghost_in_range() { 
 		return false;
 	}
 
