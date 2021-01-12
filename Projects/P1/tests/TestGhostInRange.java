bfa8ab0e8d39b3ce783faf2e8d925e735f7d85aa
Zeeshan Shaikh <zshaikh322@gmail.com>
2021-01-11 21:42:12 -0500

    Update TestGhostInRange.java

--- Projects/P1/tests/TestGhostInRange.java
+++ Projects/P1/tests/TestGhostInRange.java
@@ -5,7 +5,12 @@ import java.io.*;
 public class TestGhostInRange extends TestCase {
 
 	public void testGhostInRange() throws FileNotFoundException{
-		return null;
+		NoFrame frame = new NoFrame();
+
+		PacMan pacman = frame.addPacMan(new Location(1,1));
+		Ghost ghost = frame.addGhost(new Location(1,2), "ghost", Color.red);
+
+		assertTrue(pacman.is_ghost_in_range());
 	}
 }
 
