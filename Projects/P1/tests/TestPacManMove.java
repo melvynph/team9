a2b6419635206ccf9ac20c49a0ab5afeac49cb29
Zeeshan Shaikh <zshaikh322@gmail.com>
2021-01-14 20:14:00 -0500

    fixed small errors in pathing; oversight

--- Projects/P1/tests/TestPacManMove.java
+++ Projects/P1/tests/TestPacManMove.java
@@ -5,18 +5,6 @@ import java.io.*;
 public class TestPacManMove extends TestCase {
 
 	public void testPacManMove() throws FileNotFoundException{
-		NoFrame frame = new NoFrame(); //Creates A New Map With Walls and Tokens w/o a Display
-
-		PacMan pacman = frame.addPacMan(new Location(1,1));
-		WallComponent wall1 = new WallComponent(2,1, 20);
-		WallComponent wall2 = new WallComponent(1,2, 20);
-		//PacMan pacman = frame.addPacMan(new Location(0,0));
-		//WallComponent wall1 = new WallComponent(0,1, 20);
-		//WallComponent wall2 = new WallComponent(0,-1, 20);
-		WallComponent wall3 = new WallComponent(1,0, 20);
-		WallComponent wall4 = new WallComponent(-1,0, 20);
-		//Pacman cannot move because walls on all sides
-		assertFalse(pacman.move());
 	
 		NoFrame validMove = new NoFrame();
 		PacMan pacman2 = validMove.addPacMan(new Location(1, 1));
